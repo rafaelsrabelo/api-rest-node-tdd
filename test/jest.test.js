27d6a9dfa0e820b156c43585d170f79bdae48e36
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-10-28 09:21:13 -0300

    first commit

---
+++ test/jest.test.js
@@ -0,0 +1,17 @@
+test('Devo conhecer as principais assertivas do jest', () => {
+  let number = null;
+  expect(number).toBeNull();
+  number = 10;
+  expect(number).not.toBeNull();
+  expect(number).toBe(10);
+  expect(number).toEqual(10);
+  expect(number).toBeGreaterThan(9);
+  expect(number).toBeLessThan(11);
+});
+
+test('Devo saber trabalhar com objetos', () => {
+  const obj = { name: "Rafael", email: "rafaelrabelodev@gmail.com" };
+  expect(obj).toHaveProperty('name');
+  expect(obj).toHaveProperty('name', 'Rafael');
+  expect(obj.name).toBe('Rafael');
+});
