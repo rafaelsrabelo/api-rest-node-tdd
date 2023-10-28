ac2c1cbff09bd2ee056f96baab74b76f8c10d005
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-10-28 12:27:54 -0300

    chore(package.json): update "secure-mode" script to set verbose flag to false for jest tests feat(app.js): add new route to get list of users with a single user object test(users.test.js): add test to verify if the /users route returns a list with a single user object

--- src/app.js
+++ src/app.js
@@ -4,4 +4,13 @@ app.get('/', (req, res) => {
     res.status(200).send();
 })
 
+app.get('/users', (req, res) => {
+    const users = [
+        {
+            name: "Rafael Rabelo", email: "rafaelrabelodev@gmail.com"
+        }
+    ]
+    res.status(200).json(users);
+})
+
 module.exports = app;
