e44c793941d93416466a6175c20bcf54fa726f12
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-10-28 12:53:26 -0300

    chore(package.json): add body-parser dependency to handle JSON parsing in requests feat(app.js): add body-parser middleware to parse JSON in requests feat(app.js): add POST /users route to insert users test(users.test.js): add test case for inserting users successfully

--- test/users.test.js
+++ test/users.test.js
@@ -9,3 +9,12 @@ test('Deve listar todos os usuários', () => {
         expect(res.body[0]).toHaveProperty("name", "Rafael Rabelo")
     })
 })
+
+test('Deve inserir usuários com sucesso', () => {
+    return request(app).post('/users')
+        .send({ name: "Bernardo", email: "bernardo@gmail.com" })
+        .then(res => {
+            expect(res.status).toBe(201);
+            expect(res.body.name).toBe("Bernardo")
+        })
+})
