e90637dad1261bf1145d741d8047b05098e42ba9
Rafael Santana Rabelo <rafaelrabelo1610@gmail.com>
2023-10-30 01:14:55 -0300

    refactor(app.js): reorganize code structure for better readability and maintainability feat(app.js): add support for API versioning to improve scalability and backward compatibility

--- src/app.js
+++ src/app.js
@@ -4,35 +4,40 @@ const consign = require('consign');
 const knex = require('knex');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-      host: 'localhost',
-      database: 'rumi',
-      user: 'postgres',
-      password: 'example'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: 'src/migrations'
-    }}
+  client: 'pg',
+  connection: {
+    host: 'localhost',
+    database: 'rumi',
+    user: 'postgres',
+    password: 'example'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: 'src/migrations'
+  }
+}
 );
 
 app.db = db;
 
-consign({'cwd': 'src'})
-    .include('./config/middlewares.js')
-    .then('./services')
-    .then('./routes')
-    .then('./config/routes.js')
-    .into(app);
+const apiVersion = '/api/v1';
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-      message: "Bem vindo a rumi API"
-    });
+consign({ 'cwd': 'src' })
+  .include('./config/middlewares.js')
+  .then('./services')
+  .then('./routes')
+  .then('./config/routes.js')
+  .into(app);
+
+app.use(apiVersion, app._router);
+
+app.get(`${apiVersion}/`, (req, res) => {
+  res.status(200).json({
+    message: `Bem vindo à API da versão 1 da Rumi`
+  });
 });
 
 module.exports = app;
